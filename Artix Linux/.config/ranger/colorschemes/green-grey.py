da77aa5f743648fbb8dc906eaf9bb64153ee43e4
Proste Ja <herminamrkvova@gmail.com>
2020-04-20 12:06:30 +0000

    Upload New File

---
+++ Artix Linux/.config/ranger/colorschemes/green-grey.py
@@ -0,0 +1,159 @@
+# Ranger colorscheme
+
+from ranger.gui.colorscheme import ColorScheme
+from ranger.gui.color import *
+
+class Blue(ColorScheme):
+    progress_bar_color = 108
+
+    def use(self, context):
+        fg, bg, attr = default_colors
+
+        if context.reset:
+            return default_colors
+
+        elif context.in_browser:
+            if context.selected:
+                attr = reverse
+            else:
+                attr = normal
+            if context.empty or context.error:
+                fg = 174
+                bg = 235
+            if context.border:
+                fg = blue
+            if context.image:
+                fg = 108
+            if context.video:
+                fg = 66
+            if context.audio:
+                fg = 116
+            if context.document:
+                fg = 151
+            if context.container:
+                attr |= normal
+                fg = 174
+            if context.directory:
+                attr |= normal
+                fg = blue
+            elif context.executable and not \
+                    any((context.media, context.container,
+                       context.fifo, context.socket)):
+                attr |= normal
+                fg = 217
+            if context.socket:
+                fg = 180
+                attr |= normal
+            if context.fifo or context.device:
+                fg = 144
+                if context.device:
+                    attr |= normal
+            if context.link:
+                fg = context.good and 144 or 116
+                bg = 234
+            if context.bad:
+                bg = 235
+            if context.tag_marker and not context.selected:
+                attr |= normal
+                if fg in (174, 95):
+                    fg = blue
+                else:
+                    fg = 174
+            if not context.selected and (context.cut or context.copied):
+                fg = black
+                attr |= normal
+            if context.main_column:
+                if context.selected:
+                    attr |= normal
+                if context.marked:
+                    attr |= normal
+                    fg = yellow
+            if context.badinfo:
+                if attr & reverse:
+                    bg = magenta
+                else:
+                    fg = magenta
+
+        elif context.in_titlebar:
+            attr |= normal
+            if context.hostname:
+                fg = context.bad and red or green
+            elif context.directory:
+                fg = blue
+            elif context.tab:
+                if context.good:
+                    bg = green
+            elif context.link:
+                fg = cyan
+
+        elif context.in_statusbar:
+            if context.permissions:
+                if context.good:
+                    fg = green
+                elif context.bad:
+                    fg = magenta
+            if context.marked:
+                attr |= normal | reverse
+                fg = yellow
+            if context.message:
+                if context.bad:
+                    attr |= normal
+                    fg = red
+            if context.loaded:
+                bg = self.progress_bar_color
+            if context.vcsinfo:
+                fg = blue
+                attr &= ~bold
+            if context.vcscommit:
+                fg = yellow
+                attr &= ~bold
+            if context.vcsdate:
+                fg = cyan
+                attr &= ~bold
+
+        if context.text:
+            if context.highlight:
+                attr |= reverse
+
+        if context.in_taskview:
+            if context.title:
+                fg = blue
+
+            if context.selected:
+                attr |= reverse
+
+            if context.loaded:
+                if context.selected:
+                    fg = self.progress_bar_color
+                else:
+                    bg = self.progress_bar_color
+
+        if context.vcsfile and not context.selected:
+            attr &= ~bold
+            if context.vcsconflict:
+                fg = magenta
+            elif context.vcschanged:
+                fg = red
+            elif context.vcsunknown:
+                fg = red
+            elif context.vcsstaged:
+                fg = green
+            elif context.vcssync:
+                fg = green
+            elif context.vcsignored:
+                fg = default
+
+        elif context.vcsremote and not context.selected:
+            attr &= ~bold
+            if context.vcssync or context.vcsnone:
+                fg = green
+            elif context.vcsbehind:
+                fg = red
+            elif context.vcsahead:
+                fg = blue
+            elif context.vcsdiverged:
+                fg = magenta
+            elif context.vcsunknown:
+                fg = red
+
+        return fg, bg, attr
